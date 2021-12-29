0e3774ed4d7d0f660f483a0488504fc13c2b193e
Jorge Mendez Ortega <jorge.mendez.ortega@gmail.com>
2021-12-28 18:41:37 -0600

    fix: cambio de rutas para assets

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://orcapracticas.github.io/",
+  base: "https://orcapracticas.github.io/gh-konami12/",
   plugins: [react()]
 })
